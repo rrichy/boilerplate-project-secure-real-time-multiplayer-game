010b52010985e2685068da26093def750bf7d9fa
rrichy <rasulrichy@gmail.com>
2021-05-19 14:44:04 +0800

    update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
 
 const app = express();
+const http = require('http').createServer(app);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
@@ -31,10 +32,55 @@ app.use(function(req, res, next) {
     .send('Not Found');
 });
 
+const io = socket(http);
+let gameState = {
+  collectible: {},
+  player: {}
+}; // player details, collectible
+
+io.on('connection', client => {
+  console.log('a user connected with id: ' + client.id);
+
+  if(Object.values(gameState.collectible).length == 0) {
+    gameState.collectible = {
+      id: client.id,
+      x: Math.floor(Math.random() * 640),
+      y: Math.floor(Math.random() * 480),
+      value: 10
+    };
+  }
+
+  let playerDetail = {
+    id: client.id,
+    x: Math.floor(Math.random() * 640),
+    y: Math.floor(Math.random() * 480),
+    score: 0
+  };
+
+  gameState.player[client.id] = playerDetail;
+
+  io.emit('welcome', {id: client.id});
+
+  let interval = setInterval(() => {
+    io.emit('gameloop', gameState);
+
+
+  }, 100);
+
+  client.on('move', data => {
+    gameState.player[client.id] = data;
+  })
+
+  client.on('disconnect', () => {
+    delete gameState.player[client.id];
+    console.log(client.id + ' has disconnected');
+  });
+});
+
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-const server = app.listen(portNum, () => {
+const server = http.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
   if (process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
